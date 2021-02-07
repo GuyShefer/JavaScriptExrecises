1484a12336498569f3bdd274da6d7dea02098970
Guy <shefer.guy1@gmail.com>
2021-02-07 11:43:26 +0200

    .

---
+++ 2.3 - circle area/app.js
@@ -0,0 +1,15 @@
+(function () {
+
+
+    function calcCircleArea(radius){
+        let circleArea = Math.PI * radius * radius;
+        console.log(circleArea);
+        console.log(circleArea.toFixed(2));
+        
+    }
+
+    calcCircleArea(3);
+    calcCircleArea(5);
+    calcCircleArea(10);
+
+})();
