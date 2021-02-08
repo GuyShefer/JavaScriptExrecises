040faf1ee29a8d8bfa76bb8c7b446e4e35a65ecc
Guy <shefer.guy1@gmail.com>
2021-02-08 11:25:13 +0200

    .

---
+++ 4.5 - px to integer/app.js
@@ -0,0 +1,24 @@
+(function () {
+
+    let isLeapYear = (year) => {
+        if (year % 4 === 0) {
+            if (year % 100 === 0) {
+                if (year % 400 === 0) {
+                    console.log("It is indeed a leap year.");
+                } else {
+                    console.log("This is not a leap year.")
+                }
+            } else {
+                console.log("It is indeed a leap year.");
+            }
+        } else {
+            console.log("This is not a leap year.")
+        }
+    }
+
+    isLeapYear(2012);
+    isLeapYear(2100);
+    isLeapYear(2400);
+
+
+})();
