2dfeead5d34d65857ebde3943f8ddd3efcf4c3e0
Guy <shefer.guy1@gmail.com>
2021-02-10 09:19:40 +0200

    ...

--- 6.4 - simple for loop/app.js
+++ 6.4 - simple for loop/app.js
@@ -1,6 +1,6 @@
 (function () {
 
-     let acrobats = [5, 5, 8, -1, -1, -1, 3 ,7 , 8];
+     let acrobats = [5, 5, 8, -1, -1, -1, 3, 7, 9];
 
      let avgJumper = (arr) => {
           let eachJumperAvg = [];
@@ -15,8 +15,8 @@
                }
 
                if ((i + 1) % 3 === 0) {
-                    if(timesToDivide !== 0){
-                    eachJumperAvg.push(calcOneJumperScore / timesToDivide);
+                    if (timesToDivide !== 0) {
+                         eachJumperAvg.push(calcOneJumperScore / timesToDivide);
                     } else {
                          eachJumperAvg.push('There is Not avaiable score..');
                     }
@@ -27,7 +27,32 @@
           return eachJumperAvg;
      }
 
-     console.log(avgJumper(acrobats));
+     let jumpersAvgScoreArr = avgJumper(acrobats);
+     console.log(jumpersAvgScoreArr); //2
 
+     let bestAvgJump = (arr) => {
+          let bestAvg = 0;
+          for (let i = 0; i < arr.length; i++) {
+               if (!isNaN(arr[i])) {
+                    bestAvg = Math.max(bestAvg, arr[i]);
+               }
+          }
+          if (bestAvg === 0) {
+               return 'There is no jumpers to calculate';
+          }
+          return bestAvg;
+     }
+
+     console.log(bestAvgJump(jumpersAvgScoreArr)); //3
+
+     let bestJump = (arr) => {
+          let bestJumpValue = 0;
+          for(let i = 0; i < arr.length; i++){
+               bestJumpValue = Math.max(bestJumpValue, arr[i]);
+          }
+          return bestJumpValue;
+     }
+
+     console.log(bestJump(acrobats)); //4
 
 })();
