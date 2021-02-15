cb7db41a25e4a4de0c797e4433e07e1835721fbf
Guy <shefer.guy1@gmail.com>
2021-02-15 10:28:47 +0200

    .

---
+++ 10.3 - map vs forEach/app.js
@@ -0,0 +1,50 @@
+(function () {
+
+     const newReleases = [
+          {
+               id: 70111470,
+               title: "Die Hard",
+               boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+               uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+               rating: [4.0],
+               bookmark: [],
+          },
+          {
+               id: 654356453,
+               title: "Bad Boys",
+               boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+               uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+               rating: [5.0],
+               bookmark: [{ id: 'W432534', time: 65876586 }],
+          },
+          {
+               id: 65432445,
+               title: "The Chamber",
+               boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+               uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+               rating: [4.0],
+               bookmark: [],
+          },
+          {
+               id: 675465,
+               title: "Fracture",
+               boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+               uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+               rating: [5.0],
+               bookmark: [{ id: '432534', time: 65876586 }],
+          },
+     ];
+
+     let movieNameAndId = [];
+     newReleases.forEach((movie) => {
+          movieNameAndId.push({ id: movie.id, name:movie.title});
+     });
+
+     console.log(movieNameAndId); //1
+
+     let movieNameAndId2 = newReleases.map( movie => ({ id: movie.id, name:movie.title}));
+
+
+     console.log(movieNameAndId2)
+
+})();
