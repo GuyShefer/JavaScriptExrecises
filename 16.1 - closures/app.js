d3c96678af1fc0780d2a49854ae2452ec3f5cdaa
Guy <shefer.guy1@gmail.com>
2021-02-17 17:11:46 +0200

    .

---
+++ 16.1 - closures/app.js
@@ -0,0 +1,16 @@
+(function () {
+
+    // block 1 result : 
+    // the firstResult will return nothnig and the result will return 5
+
+    // block 2 result : 
+    // b2(); just will set the local function to 10
+    // and console.log(a); will print 1
+
+    // block 3 result :
+    // will print 333, to fix you should set the i to let.
+
+ 
+    
+
+})();
