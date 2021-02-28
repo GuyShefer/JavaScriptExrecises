48ef18710cd9d6a9769e4ce4fac9dd2d5e93f779
Guy <shefer.guy1@gmail.com>
2021-02-28 19:35:58 +0200

    .

---
+++ 28.1 - who is stronger/app.js
@@ -0,0 +1,24 @@
+(function () {
+
+    const hero = {
+        health: 5,
+        power: 68,
+        getStrength: function () {
+            if (this.health <= 5) {
+                return this.power - 10;
+            } else return this.power;
+        }
+    }
+
+    function whoIsStronger(getStrength) {
+        const myStrength = 82;
+        if (getStrength() < myStrength) {
+            return "I am stronger";
+        } else return "You are stronger";
+    }
+
+    const calc = hero.getStrength.bind(hero);
+    console.log( whoIsStronger(calc));
+
+
+})();
