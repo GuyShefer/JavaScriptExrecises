e6e05471b7ec6088cd18d2a75e84cd5978d5b6fa
Guy <shefer.guy1@gmail.com>
2021-03-01 10:41:12 +0200

    .

---
+++ 28.3 - heroes/index.js
@@ -0,0 +1,21 @@
+(function () {
+
+    const wonderWoman = {
+        name: "Diana Prince"
+    }
+    const batman = {
+        name: "Bruce Wayne"
+    }
+    const superHeroes = [wonderWoman, batman];
+
+    function printName() {
+        console.log(`my name is ${this.name}`);
+    }
+
+    function printHeroes(heroes, printFunc) {
+        heroes.forEach(x => printFunc.call(x));
+    }
+
+    printHeroes(superHeroes, printName)
+
+})();
