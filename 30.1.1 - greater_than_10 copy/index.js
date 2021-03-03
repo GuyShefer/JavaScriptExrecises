6d50b4cd6190dba22da81743852cdd3324a2fd88
Guy <shefer.guy1@gmail.com>
2021-03-03 15:07:21 +0200

    .

---
+++ 30.1.1 - greater_than_10 copy/index.js
@@ -0,0 +1,20 @@
+(function () {
+
+    let greaterThen10 = num => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                num > 10 ?
+                    resolve('Greater then 10') :
+                    reject('Less then 10');
+            }, 300);
+        }).then(
+            function (value) { console.log(value) },
+            function (error) { console.log(error) }
+        )
+    }
+
+    greaterThen10(20);
+    greaterThen10(10);
+    greaterThen10(8);
+
+})();
